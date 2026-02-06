8eb66e8963d39336ef0112f089f74c28b7ab4a76
wskang <wskang@mobaseasec.com>
2026-02-06 10:51:45 +0900

    env 환경 추가 (github Action fix error)

--- crawler.py
+++ crawler.py
@@ -5,6 +5,11 @@ from urllib.parse import quote
 from datetime import datetime
 from collections import defaultdict
 from db_mysql import save_articles
+from dotenv import load_dotenv
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent
+load_dotenv(BASE_DIR / ".env")
 
 from rules import (
     INCLUDE_SW,
